39a2f5711df8b1759277ce1ff8cad755235fc513
Paulo Silver <paulodasilva.biz@gmail.com>
2017-09-15 10:51:41 +0100

    Add files via upload

---
+++ tests/teste_slider_buttons.ino
@@ -0,0 +1,68 @@
+//vals
+static char sign = 0;  // Holds -1, 1 or 0 to turn the motor on/off and control direction
+
+//pins
+#define encoderPinA 2
+#define encoderPinB 3
+#define ZIGZAG_PIN 7 
+#define LEFT_PIN 5
+#define STOP_PIN 4
+#define RIGHT_PIN 6
+#define enabledriver 8
+
+
+void setup() {
+  //debug
+    Serial.begin(9600);  // output
+    Serial.println("start");
+  
+  //button
+  pinMode(LEFT_PIN, INPUT_PULLUP);
+  pinMode(STOP_PIN, INPUT_PULLUP);
+  pinMode(RIGHT_PIN, INPUT_PULLUP);
+  pinMode(ZIGZAG_PIN, INPUT_PULLUP);
+
+  Serial.println(digitalRead(LEFT_PIN));
+  Serial.println(digitalRead(STOP_PIN));
+  Serial.println(digitalRead(RIGHT_PIN));
+  Serial.println(digitalRead(ZIGZAG_PIN));
+
+
+}
+
+void loop() {
+
+
+  // If a switch is pushed down (low), set the sign value appropriately
+  if (digitalRead(LEFT_PIN) == 0) {
+    delay(250);
+    sign = 1;
+    // debug
+    Serial.println("-1");
+
+  }
+
+  else if (digitalRead(RIGHT_PIN) == 0) {
+    delay(250);
+    sign = -1;
+    // debug
+    Serial.println("1");
+  }
+
+  else if (digitalRead(STOP_PIN) == 0) {
+    delay(250);
+    sign = 0;
+    // debug
+    Serial.println("0");
+  }
+
+  else if (digitalRead(ZIGZAG_PIN) == 0) {
+    delay(250);
+    sign = 1;
+    // debug
+    Serial.println("ZIGZAG");
+  }
+
+
+}
+
